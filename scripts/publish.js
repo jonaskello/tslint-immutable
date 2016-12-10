6244a57a48182fcd2dc6d0acdf0cf69d3ced9e00
Jonas Kello <jonas.kello@divid.se>
2016-12-10 14:05:22 +0100

    Fix hardcoded version

--- scripts/publish.js
+++ scripts/publish.js
@@ -2,7 +2,7 @@ require("shelljs/global");
 
 const versionPart = process.argv[2];
 console.log(`Incrementing the ${versionPart} part of version in package.json...`);
-const newVersion = "1.0.0"; //incrementVersion(versionPart);
+const newVersion = incrementVersion(versionPart);
 console.log(`New version is ${newVersion}, commiting rules, package.json...`);
 commit(newVersion);
 console.log(`Tagging with ${newVersion}...`);
