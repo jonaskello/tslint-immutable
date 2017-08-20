85aa24e2ba9754e2e5f8dcd97e5562c12f09084e
M. Ian Graham <games@miangraham.com>
2017-08-21 02:04:45 +0900

    Add no-object-mutation rule

--- README.md
+++ README.md
@@ -36,6 +36,7 @@ In addition to immutable rules this project also contains a few rules for enforc
   * [readonly-keyword](#readonly-keyword)
   * [readonly-array](#readonly-array)
   * [no-let](#no-let)
+  * [no-object-mutation](#no-object-mutation)
 * [Functional style rules](#functional-style-rules)
   * [no-this](#no-this-no-class)
   * [no-class](#no-this-no-class)
@@ -163,6 +164,17 @@ const SearchResults =
     }</ul>;
 ```
 
+### no-object-mutation
+This rule prohibits syntax that mutates existing objects via assignment to or deletion of their properties. While requiring the `readonly` modifier forces declared types to be immutable, it won't stop assignment into or modification of untyped objects or external types declared under different rules. Forbidding forms like `a.b = 'c'` is one way to plug this hole. Inspired by the no-mutation rule of [eslint-plugin-immutable](https://github.com/jhusain/eslint-plugin-immutable).
+
+```typescript
+const x = {a: 1};
+
+x.foo = 'bar'; // <- Modifying properties of existing object not allowed.
+x.a += 1; // <- Modifying properties of existing object not allowed.
+delete x.a; // <- Modifying properties of existing object not allowed.
+```
+
 ## Functional style rules
 
 ### no-this, no-class
@@ -284,6 +296,7 @@ Here's a sample TSLint configuration file (tslint.json) that activates all the r
     "readonly-keyword": true,
     "readonly-array": true,
     "no-let": true,
+    "no-object-mutation": true,
 
     // Functional style rules
     "no-this": true,
---
+++ src/noObjectMutationRule.ts
@@ -0,0 +1,54 @@
+import * as ts from "typescript";
+import * as Lint from "tslint";
+
+export class Rule extends Lint.Rules.AbstractRule {
+  public static FAILURE_STRING = "Modifying properties of existing object not allowed.";
+
+  public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
+    const noObjectMutationWalker = new NoObjectMutationWalker(sourceFile, this.getOptions());
+    return this.applyWithWalker(noObjectMutationWalker);
+  }
+}
+
+const forbidObjPropOnLeftSideOf: ReadonlyArray<ts.SyntaxKind> = [
+  ts.SyntaxKind.EqualsToken,
+  ts.SyntaxKind.PlusEqualsToken,
+  ts.SyntaxKind.MinusEqualsToken,
+  ts.SyntaxKind.AsteriskEqualsToken,
+  ts.SyntaxKind.AsteriskAsteriskEqualsToken,
+  ts.SyntaxKind.SlashEqualsToken,
+  ts.SyntaxKind.PercentEqualsToken,
+  ts.SyntaxKind.LessThanLessThanEqualsToken,
+  ts.SyntaxKind.GreaterThanGreaterThanEqualsToken,
+  ts.SyntaxKind.GreaterThanGreaterThanGreaterThanEqualsToken,
+  ts.SyntaxKind.AmpersandEqualsToken,
+  ts.SyntaxKind.BarEqualsToken,
+  ts.SyntaxKind.CaretEqualsToken
+];
+
+class NoObjectMutationWalker extends Lint.RuleWalker {
+
+  public visitNode(node: ts.Node): void {
+    // No assignment with object.property on the left
+    if (node && node.kind === ts.SyntaxKind.BinaryExpression) {
+      const binExp = node as ts.BinaryExpression;
+      if (binExp.operatorToken &&
+          binExp.left.kind === ts.SyntaxKind.PropertyAccessExpression &&
+          forbidObjPropOnLeftSideOf.some(k => k === binExp.operatorToken.kind)) {
+        this.addFailure(this.createFailure(node.getStart(), node.getWidth(), Rule.FAILURE_STRING));
+      }
+    }
+
+    // No deleting object properties
+    if (node && node.kind === ts.SyntaxKind.DeleteExpression) {
+      const delExp = node as ts.DeleteExpression;
+      if (delExp.expression &&
+          delExp.expression.kind === ts.SyntaxKind.PropertyAccessExpression) {
+        this.addFailure(this.createFailure(node.getStart(), node.getWidth(), Rule.FAILURE_STRING));
+      }
+    }
+
+    super.visitNode(node);
+  }
+
+}
