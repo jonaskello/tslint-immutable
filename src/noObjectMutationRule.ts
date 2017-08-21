e5c34f8d567fa2e0ef2013baf21a38f27052f4a0
M. Ian Graham <games@miangraham.com>
2017-08-22 05:36:52 +0900

    Add missing patterns to no-object-mutation (element access, inc ops)

--- src/noObjectMutationRule.ts
+++ src/noObjectMutationRule.ts
@@ -10,6 +10,11 @@ export class Rule extends Lint.Rules.AbstractRule {
   }
 }
 
+const objPropAccessors: ReadonlyArray<ts.SyntaxKind> = [
+  ts.SyntaxKind.ElementAccessExpression,
+  ts.SyntaxKind.PropertyAccessExpression
+];
+
 const forbidObjPropOnLeftSideOf: ReadonlyArray<ts.SyntaxKind> = [
   ts.SyntaxKind.EqualsToken,
   ts.SyntaxKind.PlusEqualsToken,
@@ -26,14 +31,18 @@ const forbidObjPropOnLeftSideOf: ReadonlyArray<ts.SyntaxKind> = [
   ts.SyntaxKind.CaretEqualsToken
 ];
 
+const forbidUnaryOps: ReadonlyArray<ts.SyntaxKind> = [
+  ts.SyntaxKind.PlusPlusToken,
+  ts.SyntaxKind.MinusMinusToken
+];
+
 class NoObjectMutationWalker extends Lint.RuleWalker {
 
   public visitNode(node: ts.Node): void {
     // No assignment with object.property on the left
     if (node && node.kind === ts.SyntaxKind.BinaryExpression) {
       const binExp = node as ts.BinaryExpression;
-      if (binExp.operatorToken &&
-          binExp.left.kind === ts.SyntaxKind.PropertyAccessExpression &&
+      if (objPropAccessors.some(k => k === binExp.left.kind) &&
           forbidObjPropOnLeftSideOf.some(k => k === binExp.operatorToken.kind)) {
         this.addFailure(this.createFailure(node.getStart(), node.getWidth(), Rule.FAILURE_STRING));
       }
@@ -42,8 +51,25 @@ class NoObjectMutationWalker extends Lint.RuleWalker {
     // No deleting object properties
     if (node && node.kind === ts.SyntaxKind.DeleteExpression) {
       const delExp = node as ts.DeleteExpression;
-      if (delExp.expression &&
-          delExp.expression.kind === ts.SyntaxKind.PropertyAccessExpression) {
+      if (objPropAccessors.some(k => k === delExp.expression.kind)) {
+        this.addFailure(this.createFailure(node.getStart(), node.getWidth(), Rule.FAILURE_STRING));
+      }
+    }
+
+    // No prefix inc/dec
+    if (node && node.kind === ts.SyntaxKind.PrefixUnaryExpression) {
+      const preExp = node as ts.PrefixUnaryExpression;
+      if (objPropAccessors.some(k => k === preExp.operand.kind) &&
+          forbidUnaryOps.some(o => o === preExp.operator)) {
+        this.addFailure(this.createFailure(node.getStart(), node.getWidth(), Rule.FAILURE_STRING));
+      }
+    }
+
+    // No postfix inc/dec
+    if (node && node.kind === ts.SyntaxKind.PostfixUnaryExpression) {
+      const postExp = node as ts.PostfixUnaryExpression;
+      if (objPropAccessors.some(k => k === postExp.operand.kind) &&
+         forbidUnaryOps.some(o => o === postExp.operator)) {
         this.addFailure(this.createFailure(node.getStart(), node.getWidth(), Rule.FAILURE_STRING));
       }
     }
