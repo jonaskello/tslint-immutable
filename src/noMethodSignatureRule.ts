dad99948e7b7a6ccc02c6b8b747d3e4b014448f0
Jonas Kello <jonas.kello@divid.se>
2017-09-23 18:50:07 +0200

    Rename

--- src/noMethodSignatureRule.ts
+++ src/noMethodSignatureRule.ts
@@ -5,21 +5,17 @@ export class Rule extends Lint.Rules.AbstractRule {
   public static FAILURE_STRING = "Method signature is mutable, use property signature with readonly modifier instead.";
 
   public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
-    const noThisKeywordWalker = new NoThisWalker(sourceFile, this.getOptions());
+    const noThisKeywordWalker = new NoMethodSignatureWalker(
+      sourceFile,
+      this.getOptions()
+    );
     return this.applyWithWalker(noThisKeywordWalker);
   }
 }
 
-class NoThisWalker extends Lint.RuleWalker {
+class NoMethodSignatureWalker extends Lint.RuleWalker {
   public visitNode(node: ts.Node): void {
     if (node && node.kind === ts.SyntaxKind.MethodSignature) {
-      // this.addFailure(
-      //   this.createFailure(
-      //     node.getStart(),
-      //     node.getWidth(),
-      //     Rule.FAILURE_STRING
-      //   )
-      // );
       this.addFailureAtNode(node, Rule.FAILURE_STRING);
     }
     super.visitNode(node);
