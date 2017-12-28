7cbf0da15d92855236fb9a231662cd79c26e1425
Jonas Kello <jonas.kello@divid.se>
2017-12-28 14:44:28 +0100

    Fix #60

--- src/noMixedInterfaceRule.ts
+++ src/noMixedInterfaceRule.ts
@@ -1,49 +1,53 @@
 import * as ts from "typescript";
 import * as Lint from "tslint";
+import * as Shared from "./readonly-shared";
+
+const FAILURE_STRING = "Only the same kind of members allowed in interfaces.";
 
 export class Rule extends Lint.Rules.AbstractRule {
-  public static FAILURE_STRING = "Only the same kind of members allowed in interfaces.";
-
   public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
-    const walker = new PropertyInterfaceWalker(sourceFile, this.getOptions());
-    return this.applyWithWalker(walker);
+    return this.applyWithFunction(
+      sourceFile,
+      (ctx: Lint.WalkContext<Shared.Options>) =>
+        Shared.walk(ctx, checkNode, FAILURE_STRING),
+      Shared.parseOptions(this.ruleArguments)
+    );
   }
 }
 
-class PropertyInterfaceWalker extends Lint.RuleWalker {
-  public visitInterfaceDeclaration(node: ts.InterfaceDeclaration): void {
-    // Extract 'kind' from all members to a list of numbers.
-    const memberKinds: number[] = node.members.map(m => m.kind);
-
-    // Check so all members of a node have the same kind,
-    const unUniqueMember: number = uniqIndex(memberKinds);
-    if (unUniqueMember !== -1) {
-      this.addFailure(
-        this.createFailure(
-          node.members[unUniqueMember].getStart(),
-          node.members[unUniqueMember].getWidth(),
-          Rule.FAILURE_STRING
-        )
-      );
+function checkNode(
+  node: ts.Node,
+  _ctx: Lint.WalkContext<Shared.Options>
+): ReadonlyArray<Shared.InvalidNode> {
+  const invalidNodes = [];
+  if (node.kind === ts.SyntaxKind.InterfaceDeclaration) {
+    const interfaceDeclaration = node as ts.InterfaceDeclaration;
+    let prevMemberKind: number | undefined = undefined;
+    let prevMemberType: number | undefined = undefined;
+    for (const member of interfaceDeclaration.members) {
+      const memberKind = member.kind;
+      let memberType = 0;
+      // If it is a property declaration we need to check the type too
+      if (member.kind === ts.SyntaxKind.PropertySignature) {
+        const propertySignature = member as ts.PropertySignature;
+        // Special care for function type
+        if (
+          propertySignature.type &&
+          propertySignature.type.kind === ts.SyntaxKind.FunctionType
+        ) {
+          // We only set memberType for Functions
+          memberType = propertySignature.type.kind;
+        }
+      }
+      if (
+        prevMemberKind !== undefined &&
+        (prevMemberKind !== memberKind || prevMemberType !== memberType)
+      ) {
+        invalidNodes.push(Shared.createInvalidNode(member));
+      }
+      prevMemberKind = memberKind;
+      prevMemberType = memberType;
     }
-
-    super.visitInterfaceDeclaration(node);
   }
-}
-
-/**
- * Return the index of the first non unique item.
- *
- */
-function uniqIndex(list: number[]): number {
-  let i = 0;
-  let lastItem: number | undefined = undefined;
-  for (let item of list) {
-    if (lastItem !== undefined && lastItem !== item) {
-      return i;
-    }
-    i++;
-    lastItem = item;
-  }
-  return -1;
+  return invalidNodes;
 }
