5679a35f7305583438ab6991160a4cbeb211416e
Jonas Kello <jonas.kello@divid.se>
2017-12-29 00:54:54 +0100

    refactor no-object-mutation

--- src/noObjectMutationRule.ts
+++ src/noObjectMutationRule.ts
@@ -1,5 +1,11 @@
 import * as ts from "typescript";
 import * as Lint from "tslint";
+import {
+  createInvalidNode,
+  CheckNodeResult,
+  createCheckNodeRule,
+  InvalidNode
+} from "./shared/check-node";
 
 const OPTION_IGNORE_PREFIX = "ignore-prefix";
 
@@ -20,17 +26,12 @@ function parseOptions(options: any[]): Options {
   return { ignorePrefix };
 }
 
-export class Rule extends Lint.Rules.AbstractRule {
-  public static FAILURE_STRING = "Modifying properties of existing object not allowed.";
-
-  public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
-    const noObjectMutationWalker = new NoObjectMutationWalker(
-      sourceFile,
-      this.getOptions()
-    );
-    return this.applyWithWalker(noObjectMutationWalker);
-  }
-}
+// tslint:disable-next-line:variable-name
+export const Rule = createCheckNodeRule(
+  checkNode,
+  "Modifying properties of existing object not allowed.",
+  parseOptions
+);
 
 const objPropAccessors: ReadonlyArray<ts.SyntaxKind> = [
   ts.SyntaxKind.ElementAccessExpression,
@@ -58,102 +59,86 @@ const forbidUnaryOps: ReadonlyArray<ts.SyntaxKind> = [
   ts.SyntaxKind.MinusMinusToken
 ];
 
-class NoObjectMutationWalker extends Lint.RuleWalker {
-  ignorePrefix: string | string[] | undefined;
+function checkNode(
+  node: ts.Node,
+  ctx: Lint.WalkContext<Options>
+): CheckNodeResult {
+  let invalidNodes: Array<InvalidNode> = [];
 
-  constructor(sourceFile: ts.SourceFile, options: Lint.IOptions) {
-    super(sourceFile, options);
-    Object.assign(this, parseOptions(options.ruleArguments));
+  // No assignment with object.property on the left
+  if (node && node.kind === ts.SyntaxKind.BinaryExpression) {
+    const binExp = node as ts.BinaryExpression;
+    if (
+      objPropAccessors.some(k => k === binExp.left.kind) &&
+      forbidObjPropOnLeftSideOf.some(k => k === binExp.operatorToken.kind) &&
+      !isIgnored(binExp.getText(node.getSourceFile()), ctx.options.ignorePrefix)
+    ) {
+      invalidNodes = [...invalidNodes, createInvalidNode(node)];
+    }
   }
 
-  public visitNode(node: ts.Node): void {
-    // No assignment with object.property on the left
-    if (node && node.kind === ts.SyntaxKind.BinaryExpression) {
-      const binExp = node as ts.BinaryExpression;
-      if (
-        objPropAccessors.some(k => k === binExp.left.kind) &&
-        forbidObjPropOnLeftSideOf.some(k => k === binExp.operatorToken.kind) &&
-        !this.isIgnored(binExp.getText(this.getSourceFile()))
-      ) {
-        this.addFailure(
-          this.createFailure(
-            node.getStart(),
-            node.getWidth(),
-            Rule.FAILURE_STRING
-          )
-        );
-      }
+  // No deleting object properties
+  if (node && node.kind === ts.SyntaxKind.DeleteExpression) {
+    const delExp = node as ts.DeleteExpression;
+    if (
+      objPropAccessors.some(k => k === delExp.expression.kind) &&
+      !isIgnored(
+        delExp.expression.getText(node.getSourceFile()),
+        ctx.options.ignorePrefix
+      )
+    ) {
+      invalidNodes = [...invalidNodes, createInvalidNode(node)];
     }
-
-    // No deleting object properties
-    if (node && node.kind === ts.SyntaxKind.DeleteExpression) {
-      const delExp = node as ts.DeleteExpression;
-      if (
-        objPropAccessors.some(k => k === delExp.expression.kind) &&
-        !this.isIgnored(delExp.expression.getText(this.getSourceFile()))
-      ) {
-        this.addFailure(
-          this.createFailure(
-            node.getStart(),
-            node.getWidth(),
-            Rule.FAILURE_STRING
-          )
-        );
-      }
-    }
-
-    // No prefix inc/dec
-    if (node && node.kind === ts.SyntaxKind.PrefixUnaryExpression) {
-      const preExp = node as ts.PrefixUnaryExpression;
-      if (
-        objPropAccessors.some(k => k === preExp.operand.kind) &&
-        forbidUnaryOps.some(o => o === preExp.operator) &&
-        !this.isIgnored(preExp.operand.getText(this.getSourceFile()))
-      ) {
-        this.addFailure(
-          this.createFailure(
-            node.getStart(),
-            node.getWidth(),
-            Rule.FAILURE_STRING
-          )
-        );
-      }
-    }
-
-    // No postfix inc/dec
-    if (node && node.kind === ts.SyntaxKind.PostfixUnaryExpression) {
-      const postExp = node as ts.PostfixUnaryExpression;
-      if (
-        objPropAccessors.some(k => k === postExp.operand.kind) &&
-        forbidUnaryOps.some(o => o === postExp.operator) &&
-        !this.isIgnored(postExp.getText(this.getSourceFile()))
-      ) {
-        this.addFailure(
-          this.createFailure(
-            node.getStart(),
-            node.getWidth(),
-            Rule.FAILURE_STRING
-          )
-        );
-      }
-    }
-
-    super.visitNode(node);
   }
 
-  private isIgnored(text: string): boolean {
-    if (!this.ignorePrefix) {
-      return false;
+  // No prefix inc/dec
+  if (node && node.kind === ts.SyntaxKind.PrefixUnaryExpression) {
+    const preExp = node as ts.PrefixUnaryExpression;
+    if (
+      objPropAccessors.some(k => k === preExp.operand.kind) &&
+      forbidUnaryOps.some(o => o === preExp.operator) &&
+      !isIgnored(
+        preExp.operand.getText(node.getSourceFile()),
+        ctx.options.ignorePrefix
+      )
+    ) {
+      invalidNodes = [...invalidNodes, createInvalidNode(node)];
     }
-    if (Array.isArray(this.ignorePrefix)) {
-      if (this.ignorePrefix.find(pfx => text.indexOf(pfx) === 0)) {
-        return true;
-      }
-    } else {
-      if (text.indexOf(this.ignorePrefix) === 0) {
-        return true;
-      }
+  }
+
+  // No postfix inc/dec
+  if (node && node.kind === ts.SyntaxKind.PostfixUnaryExpression) {
+    const postExp = node as ts.PostfixUnaryExpression;
+    if (
+      objPropAccessors.some(k => k === postExp.operand.kind) &&
+      forbidUnaryOps.some(o => o === postExp.operator) &&
+      !isIgnored(
+        postExp.getText(node.getSourceFile()),
+        ctx.options.ignorePrefix
+      )
+    ) {
+      invalidNodes = [...invalidNodes, createInvalidNode(node)];
     }
+  }
+
+  return { invalidNodes };
+}
+
+function isIgnored(
+  text: string,
+  ignorePrefix: Array<string> | string | undefined
+): boolean {
+  if (!ignorePrefix) {
     return false;
   }
+  if (Array.isArray(ignorePrefix)) {
+    if (ignorePrefix.find(pfx => text.indexOf(pfx) === 0)) {
+      return true;
+    }
+  } else {
+    if (text.indexOf(ignorePrefix) === 0) {
+      return true;
+    }
+  }
+  return false;
 }
