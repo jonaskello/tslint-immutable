22663c31a65485b5d9e98b36ac15b79bf2f55472
Rebecca Stevens <rebeccastevens@catalyst.net.nz>
2018-11-07 17:39:04 +1300

    Updated noClassRule

--- src/noClassRule.ts
+++ src/noClassRule.ts
@@ -18,11 +18,7 @@ function checkNode(
   node: ts.Node,
   _ctx: Lint.WalkContext<Options>
 ): CheckNodeResult {
-  return node &&
-    (node.kind === ts.SyntaxKind.ClassExpression ||
-      node.kind === ts.SyntaxKind.ClassDeclaration)
+  return ts.isClassLike(node)
     ? { invalidNodes: [createInvalidNode(node, [])] }
-    : {
-        invalidNodes: []
-      };
+    : { invalidNodes: [] };
 }
