72e3977838d70ad2c56bde3c67201d57cbac432d
Rebecca Stevens <rebeccastevens@catalyst.net.nz>
2018-11-12 00:49:07 +1300

    further refactored noObjectMutationRule with use of isAssignmentExpression.

--- src/noObjectMutationRule.ts
+++ src/noObjectMutationRule.ts
@@ -7,6 +7,7 @@ import {
   InvalidNode
 } from "./shared/check-node";
 import * as Ignore from "./shared/ignore";
+import { isAccessExpression } from "./shared/check-type";
 
 type Options = Ignore.IgnorePrefixOption;
 
@@ -15,27 +16,6 @@ export const Rule = createCheckNodeRule(
   checkNode,
   "Modifying properties of existing object not allowed."
 );
-
-const objPropAccessors: ReadonlyArray<ts.SyntaxKind> = [
-  ts.SyntaxKind.ElementAccessExpression,
-  ts.SyntaxKind.PropertyAccessExpression
-];
-
-const forbidObjPropOnLeftSideOf: ReadonlyArray<ts.SyntaxKind> = [
-  ts.SyntaxKind.EqualsToken,
-  ts.SyntaxKind.PlusEqualsToken,
-  ts.SyntaxKind.MinusEqualsToken,
-  ts.SyntaxKind.AsteriskEqualsToken,
-  ts.SyntaxKind.AsteriskAsteriskEqualsToken,
-  ts.SyntaxKind.SlashEqualsToken,
-  ts.SyntaxKind.PercentEqualsToken,
-  ts.SyntaxKind.LessThanLessThanEqualsToken,
-  ts.SyntaxKind.GreaterThanGreaterThanEqualsToken,
-  ts.SyntaxKind.GreaterThanGreaterThanGreaterThanEqualsToken,
-  ts.SyntaxKind.AmpersandEqualsToken,
-  ts.SyntaxKind.BarEqualsToken,
-  ts.SyntaxKind.CaretEqualsToken
-];
 
 const forbidUnaryOps: ReadonlyArray<ts.SyntaxKind> = [
   ts.SyntaxKind.PlusPlusToken,
@@ -51,8 +31,8 @@ function checkNode(
   // No assignment with object.property on the left
   if (
     ts.isBinaryExpression(node) &&
-    objPropAccessors.some(k => k === node.left.kind) &&
-    forbidObjPropOnLeftSideOf.some(k => k === node.operatorToken.kind) &&
+    isAccessExpression(node.left) &&
+    ts.isAssignmentExpression(node, false) &&
     !Ignore.isIgnoredPrefix(
       node.getText(node.getSourceFile()),
       ctx.options.ignorePrefix
@@ -65,7 +45,7 @@ function checkNode(
   // No deleting object properties
   if (
     ts.isDeleteExpression(node) &&
-    objPropAccessors.some(k => k === node.expression.kind) &&
+    isAccessExpression(node.expression) &&
     !Ignore.isIgnoredPrefix(
       node.expression.getText(node.getSourceFile()),
       ctx.options.ignorePrefix
@@ -77,7 +57,7 @@ function checkNode(
   // No prefix inc/dec
   if (
     ts.isPrefixUnaryExpression(node) &&
-    objPropAccessors.some(k => k === node.operand.kind) &&
+    isAccessExpression(node.operand) &&
     forbidUnaryOps.some(o => o === node.operator) &&
     !Ignore.isIgnoredPrefix(
       node.operand.getText(node.getSourceFile()),
@@ -90,7 +70,7 @@ function checkNode(
   // No postfix inc/dec
   if (
     ts.isPostfixUnaryExpression(node) &&
-    objPropAccessors.some(k => k === node.operand.kind) &&
+    isAccessExpression(node.operand) &&
     forbidUnaryOps.some(o => o === node.operator) &&
     !Ignore.isIgnoredPrefix(
       node.getText(node.getSourceFile()),
